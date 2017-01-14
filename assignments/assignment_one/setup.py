1168326f7dafa0b60c7848395e9bc153ed3de8b5
Derek Goddeau <dgodd001@odu.edu>
2017-01-14 16:13:39 -0500

    override setup.py `clean` to clean up eggs & *.pyc

--- assignments/assignment_one/setup.py
+++ assignments/assignment_one/setup.py
@@ -1,7 +1,25 @@
 #!/usr/bin/env python
-from setuptools import setup
+import os
+from setuptools import setup, Command
 
-setup(
-    setup_requires=['pytest-runner'],
-    tests_require=['pytest']
-    )
+class Clean(Command):
+    """ Remove temp files in project root
+
+    """
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        os.system('rm -rfv ./*.egg-info ./*.egg ./common_house_spider/*.pyc')
+
+
+setup(name="spider",
+      setup_requires=['pytest-runner'],
+      tests_require=['pytest'],
+      cmdclass={ 'clean' : Clean }
+      )
