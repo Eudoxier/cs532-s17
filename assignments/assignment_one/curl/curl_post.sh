0f678fd93c1976cec72eaa533fdbf66bbbeebe59
Derek Goddeau <dgodd001@odu.edu>
2017-01-22 12:50:33 -0500

    take address input

--- assignments/assignment_one/curl/curl_post.sh
+++ assignments/assignment_one/curl/curl_post.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 curl -L -i -o results.html \
-           -d 'search_theme_form=car' \
-           -d 'op=Search' \
-           -d 'form_build_id=form-6SkwdjCka872mUDOLyJspWzIHtkBGso7f5RMZ2fGr9U' \
-           -d 'form_id=search_theme_form' \
+           -d "search_theme_form=$1" \
+           -d "op=Search" \
+           -d "form_build_id=form-6SkwdjCka872mUDOLyJspWzIHtkBGso7f5RMZ2fGr9U" \
+           -d "form_id=search_theme_form" \
            https://www.nostarch.com/
