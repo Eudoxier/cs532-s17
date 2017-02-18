0e8b1eb1cf5d1507c3bc73434b7cf7249264fdc3
Derek Goddeau <dgodd001@odu.edu>
2017-02-18 15:15:44 -0500

    init script to fetch HTML from a list of links

---
+++ assignments/assignment_three/get_html.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#
+# Fetch the HTML for the links in an input file,
+# the file should have one link per line.
+
+FAILURES=0
+
+usage() {
+    echo >&2
+
+    echo >&2 "Usage: $0 file"
+}
+
+warning() {
+    echo >&2    '[*] Warning: this script will overwrite data in data/raw_html waiting 10 seconds before starting.'
+    echo >&2
+
+    sleep 10
+}
+
+fetch() {
+    while read line; do
+        wget -O data/raw_html/$line $line
+        if [[ "$?" != 0 ]]; then
+            echo >&2 '[*] Error downloading file'
+            FAILURES=$(expr FAILURES + !)
+        else
+            echo >&2 '[*] Success'
+        fi
+    done < "$FILE"
+}
+
+stats() {
+    echo >&2 "[*] Finished with $FAILURES unsuccessful downloads."
+}
+
+if [ $# != 1 ] ; then 
+    usage
+    exit 1
+else
+    FILE=$1
+    echo >&2
+    echo >&2 "[*] Fetching HTML for links in file $FILE"
+fi
+
+warning
+fetch
+stats
+
+exit 0
